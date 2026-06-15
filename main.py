501f00b7366689ed83e63d4fc73628af8a71e81f
PiyushPrasad9798 <piyushpd9798@gmail.com>
2026-06-15 08:20:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("All the basic python code")
+st.logo("logo.png")
+st.image("logo.png")
+t1=st.text_input("USERNAME")
+t2=st.text_input("PASSWORD",type="password")
+b1=st.button("SignIn")
