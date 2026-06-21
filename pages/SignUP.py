6b68813a65ba5c244ec1b428e7f4092f434b3c3a
PiyushPrasad9798 <piyushpd9798@gmail.com>
2026-06-21 23:12:14 +0530

    Update SignUP.py

--- pages/SignUP.py
+++ pages/SignUP.py
@@ -4,17 +4,17 @@ conn=mysql.connector.connect(host="127.0.0.1",user="root",database="streamlit_te
 my=conn.cursor()
 from background import set_bg
 set_bg()
-st.title("Sign Up")
-t1=st.text_input("Username")
-t2=st.text_input("Password",type="password")
-t3=st.text_input("Mobile Number")
-t4=st.text_input("Email Id")
-t5=st.date_input("DOB")
+st.title("📝 Sign Up 💻")
+t1=st.text_input("🙋‍♂️ Username")
+t2=st.text_input("🔑 Password",type="password")
+t3=st.text_input("📞 Mobile Number")
+t4=st.text_input("📩 Email Id")
+t5=st.date_input("🐣 DOB")
 b1=st.button("SIGNUP")
 if b1:
        my.execute("insert into user_info values("+"'"+t1+"'"+","+"'"+t2+"'"+","+"'"+t3+"'"+","+"'"+t4+"'"+","+"'"+str(t5)+"'"+")")
        conn.commit()
-       st.success("Sign Up Successfully")
+       st.success("✅ Sign Up Successfull")
 code='''
 import streamlit as st
 import mysql.connector
