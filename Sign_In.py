ecb8363d0ef6159ca19dd84242589a77a33938a6
PiyushPrasad9798 <piyushpd9798@gmail.com>
2026-06-21 23:16:29 +0530

    Update Sign_In.py

--- Sign_In.py
+++ Sign_In.py
@@ -43,19 +43,19 @@ col1, col2, col3 = st.columns([3,2,3])
 with col2:
     st.image("logo.jpg", width=200)
 
-    t1 = st.text_input("USERNAME")
-    t2 = st.text_input("PASSWORD", type="password")
+    t1 = st.text_input("📝 USERNAME 👤")
+    t2 = st.text_input("🔑 PASSWORD 🔐", type="password")
 
     b1 = st.button("Sign In")
     #b2 = st.button("Forget Password")
 if b1:
     if t1=="":
-       st.error("Please enter a Username and try again")
+       st.error("⚠️ Please enter a Username and try again ⚠️")
     elif t2=="":
-       st.error("Please enter your Password and try again")
+       st.error("⚠️ Please enter your Password and try again ⚠️")
     else:
-       st.success(f"Welcome {t1}!")
-       st.toast("Login Successful 🚀")
+       st.success(f"😊 Welcome {t1}!")
+       st.toast("✅ Login Successful 🚀")
        st.balloons()
 code='''
 import streamlit as st
@@ -103,20 +103,20 @@ col1, col2, col3 = st.columns([3,2,3])
 with col2:
     st.image("logo.jpg", width=200)
 
-    t1 = st.text_input("USERNAME")
-    t2 = st.text_input("PASSWORD", type="password")
+    t1 = st.text_input("📝 USERNAME 👤")
+    t2 = st.text_input("🔑 PASSWORD 🔐", type="password")
 
     b1 = st.button("Sign In")
     #b2 = st.button("Forget Password")
 if b1:
-    st.toast("Login Successful 🚀")
-    st.balloons()
     if t1=="":
-       st.error("Please enter a Username and try again")
+       st.error("⚠️ Please enter a Username and try again ⚠️")
     elif t2=="":
-       st.error("Please enter your Password and try again")
+       st.error("⚠️ Please enter your Password and try again ⚠️")
     else:
-       st.success(f"Welcome {t1}!")
+       st.success(f"😊 Welcome {t1}!")
+       st.toast("✅ Login Successful 🚀")
+       st.balloons()
 '''
 with st.expander("Show Python Code"):
     st.code(code, language="python")
