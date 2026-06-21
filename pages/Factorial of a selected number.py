76ce7e0d7c0418cc392e3a307afa1d9d680064f4
PiyushPrasad9798 <piyushpd9798@gmail.com>
2026-06-21 23:33:52 +0530

    Update Factorial of a selected number.py

--- pages/Factorial of a selected number.py
+++ pages/Factorial of a selected number.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from background import set_bg
 set_bg()
-st.title("Factorial python code")
+st.title("👨‍💻 Factorial python code </>")
 n=st.slider("Pick a number",1,100)
 f=1
 for i in range(1,n+1):
     f=f*i
 st.success(f"Factoral of {n} Is {f}")
-st.subheader("Python Code")
+st.subheader("</> Python Code </>")
 code='''
 import streamlit as st
 from background import set_bg
@@ -19,5 +19,5 @@ for i in range(1,n+1):
     f=f*i
 st.success(f"Factoral of {n} Is {f}")
 st.subheader("Python Code")'''
-with st.expander("Show Python Code"):
+with st.expander("</> Show Python Code </>"):
     st.code(code, language="python")
